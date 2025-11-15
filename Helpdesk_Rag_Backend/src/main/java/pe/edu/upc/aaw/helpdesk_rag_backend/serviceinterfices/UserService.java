f2a96f3ee7484d79b2deadebe7db6ed688062ae6
Alvaro1503 <81375924+Alvaro1503@users.noreply.github.com>
2025-11-15 09:45:00 -0500

    Add files via upload

---
+++ Helpdesk_Rag_Backend/src/main/java/pe/edu/upc/aaw/helpdesk_rag_backend/serviceinterfices/UserService.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.aaw.helpdesk_rag_backend.serviceinterfices;
+
+import pe.edu.upc.aaw.helpdesk_rag_backend.entities.Users;
+
+import java.util.List;
+
+public interface UserService {
+    public void insert(Users users);
+
+    public List<Users> list();
+
+    public void delete(Long idusers);
+
+    public Users listarId(Long idusers);
+
+    public void insertarRol(String rol, Long userId);
+}
